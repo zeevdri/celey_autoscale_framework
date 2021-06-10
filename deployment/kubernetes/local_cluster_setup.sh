f4b73b082d3c5a2b9d102273d0668b4df0497acc
zeevdri <zeev@percepto.co>
2021-06-10 20:12:39 +0300

    kubernetes:   - updated broker/results names   - moved deployment to namespace celery-autoscale   - moved broker conf to secret   - implemented TriggerAuthentication for autoscalers   - updated local_cluster_setup.sh skaffold.yaml accordingly

--- deployment/kubernetes/local_cluster_setup.sh
+++ deployment/kubernetes/local_cluster_setup.sh
@@ -8,10 +8,17 @@ GPU_NODE="minikube"  # TODO change to appropriate note when minikube multi-nodes
 
 # taint is not valid for the moment until minikube --nodes 2 will work with local docker image
 #kubectl taint nodes $GPU_NODE gpu:NoSchedule
-
+kubectl create namespace celery-autoscale
 kubectl label nodes $GPU_NODE gpu=1
 
-kubectl apply -f deployment/kubernetes/rabbitmq.yml -f deployment/kubernetes/redis.yml
+eval "$(minikube docker-env)"
+WORKER_VERSION=0.1
+DOCKER_BUILDKIT=1 docker build . -t celery-worker:latest -t "celery-worker:$WORKER_VERSION"
+
+kubectl apply -f deployment/kubernetes/celery-broker.yml -f deployment/kubernetes/celery-results.yml
+kubectl apply -f deployment/kubernetes/default-worker.yml -f deployment/kubernetes/gpu-worker.yml
+kubectl apply -f deployment/kubernetes/autoscaler-auth.yml
+kubectl apply -f deployment/kubernetes/default-worker-autoscaler.yml -f deployment/kubernetes/gpu-worker-autoscaler.yml
 
 eval "$(minikube docker-env)"
 WORKER_VERSION=0.1
