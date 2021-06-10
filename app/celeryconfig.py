20bccb311256e00aa3614452796c7ab25239ffa9
zeevdri <zeev@percepto.co>
2021-06-10 10:21:01 +0300

    kubernetes:   - initial implementation for acks_late, needs to complete to autoscaler work with rabbitmq http

--- app/celeryconfig.py
+++ app/celeryconfig.py
@@ -5,6 +5,8 @@ import kombu
 broker_url = os.environ["CELERY_BROKER_URL"]
 result_backend = os.environ["CELERY_RESULTS_BACKEND"]
 
+task_acks_late = True
+
 task_queues = (
     kombu.Queue("default", kombu.Exchange("default"), routing_key="default"),
     kombu.Queue("gpu", kombu.Exchange("gpu"), routing_key="gpu"),
