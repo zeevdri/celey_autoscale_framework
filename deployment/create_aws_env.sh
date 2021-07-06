d5837d8ad518f9bc34e0d1ec576b94a6fe52e79d
zeevdri <zeev@percepto.co>
2021-07-06 08:54:54 +0300

    create_aws_env.sh - initial create vpc - initial create subnet - initial create results backend

--- deployment/create_aws_env.sh
+++ deployment/create_aws_env.sh
@@ -2,6 +2,33 @@
 # this script is not run ready, only documentation for commands to run
 
 AWS_REGION=eu-west-2
+PROJECT_TAG="autoscaling_test"  # TODO add tags (project: autoscaling_test) to resources
+
+# create vpc and subnet
+VPC_CIDR="10.0.0.0/16"
+
+VPC_RESPONSE=$(aws --region "$AWS_REGION" ec2 create-vpc \
+     --cidr-block "$VPC_CIDR"
+)
+
+VPC_ID=$(echo "$VPC_RESPONSE" | jq ".Vpc.VpcId")
+
+aws --region "$AWS_REGION" ec2 delete-vpc \
+    --vpc-id "$VPC_ID"
+
+SUBNET_CIDR="10.0.0.0/24"
+
+SUBNET_RESPONSE=$(aws --region "$AWS_REGION" ec2 create-subnet \
+     --cidr-block "$SUBNET_CIDR" \
+     --vpc-id "$VPC_ID"
+)
+
+SUBNET_ID=$(echo "$SUBNET_RESPONSE"| jq ".Subnet.SubnetId")
+
+aws --region "$AWS_REGION" ec2 delete-subnet \
+    --subnet-id "$SUBNET_ID"
+
+# TODO maybe create subnet group
 
 # init broker
 BROKER_NAME="celery_autoscale_broker"
@@ -13,6 +40,7 @@ BROKER_RESPONSE=$(aws --region "$AWS_REGION" mq create-broker \
     --engine-type RABBITMQ \
     --engine-version "3.8.11" \
     --host-instance-type "mq.t3.micro" \
+    --subnet-ids "$SUBNET_ID"\
     --deployment-mode SINGLE_INSTANCE \
     --users "[{\"Username\": \"$BROKER_USER\", \"Password\": \"$BROKER_PASSWORD\"}]")
 
@@ -22,8 +50,22 @@ BROKER_ARN=$(echo "$BROKER_RESPONSE" | jq ".BrokerArn")
 aws --region "$AWS_REGION" mq delete-broker \
     --broker-id "$BROKER_ID"
 
+
 # init results backend
-# TODO write init results backend commands
+RESULTS_NAME="celery-autoscaling-results"
+
+
+RESULTS_RESPONSE=$(aws --region "$AWS_REGION" elasticache create-cache-cluster \
+    --cache-cluster-id "$RESULTS_NAME" \
+    --engine "redis" \
+    --cache-node-type "cache.t3.micro" \
+    --num-cache-nodes 1
+    )
+
+RESULTS_ID=$(echo "$RESULTS_RESPONSE" | jq ".CacheCluster.CacheClusterId")
+
+aws --region "$AWS_REGION" elasticache delete-cache-cluster \
+    --cache-cluster-id "$RESULTS_ID"
 
 # init kubernetes cluster
 # TODO write init kubernetes cluster commands
